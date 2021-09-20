186825f14385450fbb09e3e0743bc5f1cfe8129f
UsmanHaider <usmanhaider.falconit@gmail.com>
2021-09-20 14:55:24 +0500

    Add underconstruction starting page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,54 @@
 // Init
 import React from "react";
+import teamImg from "../assets/team.gif";
+import styled from "styled-components";
 
-// Component
+const Text = styled.p`
+  border-right: solid 3px rgba(0, 255, 0, 0.75);
+  white-space: nowrap;
+  overflow: hidden;
+  font-family: "Source Code Pro", monospace;
+  font-size: 28px;
+  color: black;
+  margin: 0 auto;
+  animation: animated-text 10s steps(29, end) 1s 1 normal both,
+    animated-cursor 600ms steps(29, end) infinite;
+  animation-iteration-count: infinite;
+  /* text animation */
+
+  @keyframes animated-text {
+    from {
+      width: 0;
+    }
+    to {
+      width: 472px;
+    }
+  }
+
+  /* cursor animations */
+
+  @keyframes animated-cursor {
+    from {
+      border-right-color: rgba(0, 255, 0, 0.75);
+    }
+    to {
+      border-right-color: transparent;
+    }
+  }
+`;
+
 export default function Header() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img src={teamImg} alt="Website is launching soon." />
+      <Text>Hi! We are Coming soon ...</Text>
     </div>
   );
 }
