045d5427a8860699b2af6e0bb0eeb37643659534
ihtisham <ihtishamhussain@falconit.com.pk>
2021-11-26 16:45:27 +0500

    changes

--- src/components/UserLandingPage/UserLandingPageAdvantages.js
+++ src/components/UserLandingPage/UserLandingPageAdvantages.js
@@ -1,13 +1,10 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Grid from '@mui/material/Grid';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { Typography } from '@mui/material';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import Button from '@mui/material/Button';
 import Carousel from "react-material-ui-carousel";
 import carouselimg from "../../assets/img/carouselimg.png";
 
@@ -19,61 +16,45 @@ function generate(element) {
   );
 }
 
-const Demo = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.background.paper,
-}));
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+
 const UserLandingPageAdvantages = () => {
- 
-    return (
-        <>
-        <Box className="box-user">  
-      <Box sx={{
-        padding:"20px 0 20px 0"
-      }}>  
-        <Typography align="center" variant="h5">
-        Advantages Passtotrip
-        </Typography>
-        <div className="userlandingpagediveider"> </div> 
-      </Box>
-      
-         <Grid container spacing={2}>
-  <Grid item xs={12} md={6}>
-  <List>
+  return (
+    <>
+      <Box className="box-user">
+        <Box className="userlandingpageheaderspacing" >
+          <Typography align="center" variant="h5">
+            Advantages Passtotrip
+          </Typography>
+          <div className="userlandingpagediveider"> </div>
+        </Box>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <List>
               {generate(
                 <ListItem>
-                  <ListItemText className="list-text"
-                    primary="Single-line item Single-line item Single-line item Single-line item Single-line item Single-line item"
-                  />
+                  <ListItemText className="list-text" primary="Single-line item Single-line item Single-line item Single-line item" />
                 </ListItem>,
               )}
             </List>
-            
-  </Grid>
-  <Grid item xs={12} md={6}>
-  <Carousel
-        autoPlay={false}
-        animation={"fade"}
-      >
-       <img className="carousel-img" src={carouselimg}/>
-       <img className="carousel-img" src={carouselimg}/>
-       <img className="carousel-img" src={carouselimg}/>
-      </Carousel>
-  </Grid>
-  
-</Grid>
-<Grid  container justifyContent="center"> 
-<button className="btn-advantages">Early access to courses</button>
-<button className="btn-advantages">Become content creator</button>
-</Grid>
-</Box> 
-        </>
-    )
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Carousel
+              autoPlay={false}
+              animation={"fade"}
+            >
+              <img className="carousel-img" src={carouselimg} />
+              <img className="carousel-img" src={carouselimg} />
+              <img className="carousel-img" src={carouselimg} />
+            </Carousel>
+          </Grid>
+        </Grid>
+        <Grid container justifyContent="center">
+          <button className="btn-advantages">Early access to courses</button>
+          <button className="btn-advantages">Become content creator</button>
+        </Grid>
+      </Box>
+    </>
+  )
 }
 
 export default UserLandingPageAdvantages
--- src/components/UserLandingPage/UserLandingPageBody.js
+++ src/components/UserLandingPage/UserLandingPageBody.js
@@ -10,14 +10,14 @@ import Aboutimg from "../../assets/img/kk.png";
 const UserLandingPageBody = () => {
     return (
         <>
-           <div className="UserLandingPageAboutContainer">
-        <div className="UserLandingPageAboutImgContainer">
+           <div className="user-landingpage-container">
+        <div className="user-landing-page-img-container">
           <img src={AboutLandingPageImg} className="aboutLeftimg" />
-          <img src={Aboutimg} className="UserLandingPageRightimg" />
+          <img src={Aboutimg} className="user-landing-page-right-img" />
         </div>
-        <div className="UserLandingPageAboutTextOnImg">
-          <h1 className="UserLandingPageAboutTextImgH1">Welcome to the Gamer's <br/> Learning 3.0 </h1>
-          <p className="UserLandingPageAboutTextOnImgP">
+        <div className="user-landing-page-text-on-img">
+          <h1 className="UserLandingPageAboutTextImgH1">Welcome to the Gamer's Learning 3.0 </h1>
+          <p className="user-landing-page-text-on-imgP">
           Join our Online Courses community and become a pro game
           </p>
           <form class="flex-form">
