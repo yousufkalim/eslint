3778b095eee361d6bbdf49172c10ab0df520c2f7
falconitconsultant <sadamramzan@falconit.com.pk>
2021-11-29 04:09:02 -0800

    remove extra code and borderradius set

--- src/containers/ContentCreatorLandingPage.js
+++ src/containers/ContentCreatorLandingPage.js
@@ -1,7 +1,4 @@
 import React from 'react'
-// import HeaderUserLandingPage from '../components/HeaderUserLandingPage'
-// import CreateHome from "../components/content/CreateHome";
-// import UserLandingPageBody from '../components/UserLandingPage/UserLandingPageBody'
 import CreateHome from '../components/ContentLending/CreateHome'
 import ContentCreator from "../components/ContentLending/ContentCreator"
 import Avantages from "../components/ContentLending/Avantages"
@@ -9,7 +6,6 @@ import Monitization from "../components/ContentLending/Monitization"
 import OurBlog from "../components/ContentLending/OurBlog"
 import Footer from "../components/blog/BlogFooter";
 import Header from "../components/Header";
-// import BlogBody from "../components/blog/BlogBody";
 
 
 const ContentCreatorLandingPage = () => {
@@ -24,11 +20,7 @@ const ContentCreatorLandingPage = () => {
          <Header />
          <CreateHome/>
          
-      {/* <BlogBody /> */}
-           {/* <HeaderUserLandingPage/> */}
-           {/* <UserLandingPageBody/> */}
-            {/* <CreateHome /> */}
-            <ContentCreator />
+                 <ContentCreator />
             <Avantages />
             <Monitization />
             <OurBlog />
