835302432d2c14be3a3fd89bfb38382aba50cbec
asfandyar <asfandyar@falconit.com.pk>
2021-11-30 10:37:08 +0500

    fix landing nav bar color

--- src/components/UserLandingPageHeader.js
+++ src/components/UserLandingPageHeader.js
@@ -3,33 +3,35 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import logo from '../assets/icons/logo.png'
-  
-const HeaderUserLandingPage = () => {
-    return (
-        <>
-           <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          > 
-          <div className="logo-text-container"> 
-           <span className="logo-text">  pass to trip </span>
-           <img className="logo-img" src={logo}/>
-          </div>
-          </Typography>
-          <Typography sx={{marginRight:"15px"}}>Blog</Typography>
-          <button className="btn-content-creator">Become Content Creator</button>
-          <button className="btn-content-creator">Get Early Access</button>
-        </Toolbar>
-      </AppBar>
-    </Box>  
-        </>
-    )
-}
+import logo from "../assets/icons/logo.png";
 
-export default HeaderUserLandingPage
+const HeaderUserLandingPage = () => {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" className="headerBackgroundColor">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              <div className="logo-text-container">
+                <span className="logo-text"> pass to trip </span>
+                <img className="logo-img" src={logo} />
+              </div>
+            </Typography>
+            <Typography sx={{ marginRight: "15px" }}>Blog</Typography>
+            <button className="btn-content-creator">
+              Become Content Creator
+            </button>
+            <button className="btn-content-creator">Get Early Access</button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+
+export default HeaderUserLandingPage;
