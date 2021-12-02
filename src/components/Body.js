7ceae078debde4c8c63eaa7384384e429803f463
falconitconsultant <sadamramzan@falconit.com.pk>
2021-12-02 04:46:18 -0800

    home design changes fix and routing login and signup form at home page

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,5 @@
 // Init
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Bodyimg from "../assets/img/body.png";
 import BodyBlurimg from "../assets/img/bodyblur.png";
 import TopCourses from "./TopCourses";
@@ -8,14 +8,23 @@ import TopRatedContent from "./TopRatedContent";
 import Request from "./Request";
 import RankingList from "./RankingList";
 import Blog from "./Blog";
+import LoginFormPopup from "./PopupForms/LoginFormPopup";
+import CreateFormPopup from "./PopupForms/CreateFormPopup";
 
 export default function Body() {
+  const [open, setOpen] = useState(false);
+  const showPopup = (e) => {
+    e.preventDefault();
+    setOpen(true);
+  };
+  const [login, setLogin] = useState(false);
   const scrollToTop = () => {
     window.scrollTo({
       top: 1,
       behavior: "smooth",
     });
   };
+
   useEffect(() => {
     scrollToTop();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -23,6 +32,20 @@ export default function Body() {
   return (
     <>
       <div className="Bodycontainer">
+        <CreateFormPopup
+          open={open}
+          setOpen={setOpen}
+          login={login}
+          setLogin={setLogin}
+        />
+        {login && (
+          <LoginFormPopup
+            open={login}
+            setOpen={setLogin}
+            signup={open}
+            setSignup={setOpen}
+          />
+        )}
         <img src={Bodyimg} alt="img" className="bodyImage" />
         <img src={BodyBlurimg} alt="img" className="blurImg" />
         <div className="textonImg">
@@ -31,7 +54,9 @@ export default function Body() {
             <br /> a pro player
           </h1>
           <p className="textonImgP">For players who want more</p>
-          <button className="textonImgbutton">Sign Up For Free</button>
+          <button className="textonImgbutton" onClick={showPopup}>
+            Sign Up For Free
+          </button>
         </div>
         <TopCourses />
         <LatestCourses />
