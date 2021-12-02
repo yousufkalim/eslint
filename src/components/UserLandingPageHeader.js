14c84e77d7e1ca333612c9d2bdad1a42a626acad
Yousuf Kalim <86219218+yousuf-falconit@users.noreply.github.com>
2021-12-02 11:09:40 +0500

    Bug fixes

--- src/components/UserLandingPageHeader.js
+++ src/components/UserLandingPageHeader.js
@@ -10,73 +10,73 @@ import SubscriptionPopup from './PopupForms/SubscriptionPopup'
 
 import { useTranslation, Trans } from "react-i18next";
 
-const HeaderUserLandingPage = () => {
-  const { t, i18n } = useTranslation();
+  
+  const HeaderUserLandingPage = () => {
+    const [open, setOpen] = useState(false)
+    const { t, i18n } = useTranslation();
 
-const HeaderUserLandingPage = () => {
-  const [open, setOpen] = useState(false)
-
-  const history = useHistory();
-  const showPopup = ()=>{
-    setOpen(true);
-  }
-  return (
-    <>
-      <Box sx={{ flexGrow: 1 }}>
-      {open && <SubscriptionPopup open={open} setOpen={setOpen}/>} 
-        <AppBar position="static" className="headerBackgroundColor">
-          <Toolbar>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-            >
-              <div
-                className="logo-text-container"
-                onClick={() => {
-                  history.push("/");
-                  window.location.reload();
+    const history = useHistory();
+    const showPopup = ()=>{
+      setOpen(true);
+    }
+    
+    return (
+      <>
+        <Box sx={{ flexGrow: 1 }}>
+        {open && <SubscriptionPopup open={open} setOpen={setOpen}/>} 
+          <AppBar position="static" className="headerBackgroundColor">
+            <Toolbar>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+              >
+                <div
+                  className="logo-text-container"
+                  onClick={() => {
+                    history.push("/");
+                    window.location.reload();
+                  }}
+                >
+                  <img className="logo-img" src={logo} />
+                </div>
+              </Typography>
+              <Link
+                to="/blog"
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                  cursor: "pointer",
                 }}
               >
-                <img className="logo-img" src={logo} />
-              </div>
-            </Typography>
-            <Link
-              to="/blog"
-              style={{
-                color: "white",
-                textDecoration: "none",
-                cursor: "pointer",
-              }}
-            >
-              <Typography sx={{ marginRight: "15px" }}>Blog</Typography>
-            </Link>
-            <Link
-              to="/contentcreator"
-              style={{ color: "white", textDecoration: "none" }}
-            >
-              <button className="btn-content-creator">
-                {t("Become Content Creator")}
-              </button>
-            </Link>
-            <Link
-              to="/userlanding"
-              style={{ color: "white", textDecoration: "none" }}
-            >
+                <Typography sx={{ marginRight: "15px" }}>Blog</Typography>
+              </Link>
+              <Link
+                to="/contentcreator"
+                style={{ color: "white", textDecoration: "none" }}
+              >
+                <button className="btn-content-creator">
+                  {t("Become Content Creator")}
+                </button>
+              </Link>
+              <Link
+                to="/userlanding"
+                style={{ color: "white", textDecoration: "none" }}
+              >
 
-              <button className="btn-content-creator">
-                {t("Get Early Access")}
-              </button>
+                <button className="btn-content-creator">
+                  {t("Get Early Access")}
+                </button>
 
-              <button onClick={showPopup} className="btn-content-creator">Get Early Access</button>
+                <button onClick={showPopup} className="btn-content-creator">Get Early Access</button>
 
-            </Link>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </>
-  );
+              </Link>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </>
+    );
 };
 
 export default HeaderUserLandingPage;
