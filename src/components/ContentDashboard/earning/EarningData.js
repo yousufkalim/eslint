d89e05b0ad25b37751c56e4918e657fd74116cc8
falconitconsultant <sadamramzan@falconit.com.pk>
2021-12-07 04:45:10 -0800

    Create dashboard earning page

---
+++ src/components/ContentDashboard/earning/EarningData.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const EarningData = () => {
+  return (
+    <>
+      <div className="earning_box_container">
+        <div className="earning_enterHeadings">
+          <p className="total_pera">Total renvenue</p>
+          <h1 className="earning_conting">$0.00</h1>
+          <p className="earnMonth_pera">$0.00 this month</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EarningData;
