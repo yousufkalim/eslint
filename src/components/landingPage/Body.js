5457ffd32af6df22c887e605dbd9d71cdbdd9050
falconitconsultant <sadamramzan@falconit.com.pk>
2021-12-08 22:49:46 -0800

    add animaiton heading body component in landing page

--- src/components/landingPage/Body.js
+++ src/components/landingPage/Body.js
@@ -9,6 +9,29 @@ export default function Body() {
   const { language } = i18n;
   console.log("t is ", language);
 
+  React.useEffect(() => {
+    document.addEventListener("onload", () => {
+      var element = document.getElementsByClassName("landingPage_heading");
+      element.classList.add("myStyles");
+      // if (element && element.length > 0) {
+      //   for (let index = 0; index < element.length; index++) {
+      //   }
+      // }
+    });
+  }, [window.onload]);
+
+  // React.useEffect(() => {
+  //   const reRender = () => {
+  //     const loads = document.getElementById("landingPage_heading");
+  //     loads.classList.add("myStyles");
+  //   };
+  //   window.onload = function () {
+  //     const loads = document.getElementById("landingPage_heading");
+  //     loads.addEventListener("click", reRender);
+  //   };
+  //   // return document.getElementById("myBtn").removeEventListener("click", reRender);
+  // }, []);
+
   return (
     <>
       <div className="landingpagecontainer">
@@ -17,16 +40,20 @@ export default function Body() {
         </div>
         <div
           className={`${
-            language == "fr" ? "frlandingpagetextonImg" : "landingpagetextonImg"
+            language == "fr"
+              ? "frlandingpagetextonImg "
+              : "landingpagetextonImg"
           }`}
         >
-          <h1 className="landingpagetextimgH1">
-            {t("Welcome to the")}
-            <br className="landingBr" /> {t("Gamer's eLearning 3.0")}
-          </h1>
-          <p className="landingpagetextonImgP">
-            {t("Join our Online Courses community and reach your goal")}
-          </p>
+          <div className="landingPage_heading ">
+            <h1 className="landingpagetextimgH1 myStyles ">
+              {t("Welcome to the")}
+              <br className="landingBr" /> {t("Gamer's eLearning 3.0")}
+            </h1>
+            <p className="myStyles ">
+              {t("Join our Online Courses community and reach your goal")}
+            </p>
+          </div>
           <div className="landingbuttondiv">
             <Link
               to="/userlanding"
