d0816a0f3ea966fa89b34f19f0400b1ab8c5c5c3
Ali-Raza-Developer <aliraza@falconit.com.pk>
2022-02-16 17:59:31 +0500

    work in top Courses api integration

--- src/components/TopCourses.js
+++ src/components/TopCourses.js
@@ -1,13 +1,33 @@
 // Init
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import Carousel from "react-material-ui-carousel";
 import { ReactComponent as Star1 } from "../assets/icons/star2.svg";
 import TopCourseImg from "../assets/img/topcourseimg.png";
 import avatar from "../assets/img/avatar.png";
 import StarIcon from "@material-ui/icons/Star";
-
+import { Store, UpdateStore } from "../../src/StoreContext";
+import api from "../api";
 export default function TopCourses() {
+  const [loading, setLoading] = useState(false);
+  // init
+  const updateStore = UpdateStore();
+  const { courses } = Store();
+
+  useEffect(() => {
+    // get top courses
+    getTopCourses();
+  }, []);
+
+  let getTopCourses = async () => {
+    setLoading(true);
+
+    let res = await api("get", "/courses");
+    if (res) {
+      updateStore({ courses: res?.data });
+    }
+    setLoading(false);
+  };
   var items = [
     {
       name: "PUBG gameplay full course",
@@ -31,7 +51,7 @@ export default function TopCourses() {
         activeIndicatorIconButtonProps={{ className: "activeIndicator" }}
         className="topcoursecarousal"
       >
-        {items.map((item, i) => (
+        {courses?.map((item, i) => (
           <TopCoursesComponent key={i} item={item} />
         ))}
       </Carousel>
@@ -40,6 +60,29 @@ export default function TopCourses() {
 }
 
 function TopCoursesComponent({ item }) {
+  let countViews = (course) => {
+    const Videos = course?.videos;
+
+    let count = 0;
+    Videos.map((video) => {
+      count += video.views;
+    });
+
+    return count;
+  };
+  let countTime = (course) => {
+    const Videos = course?.videos;
+
+    let count = 0;
+    if (Videos) {
+      Videos.map((video) => {
+        count += video.duration;
+      });
+    }
+
+    return count;
+  };
+
   return (
     <Grid
       container
@@ -55,15 +98,13 @@ function TopCoursesComponent({ item }) {
           <Grid item xs={8} md={8}>
             <h3 className="h3heading">{item?.name}</h3>
             <p className="topcourseText">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa
-              semper mi feugiat quis tellus arcu. Rutrum non vitae id urna nunc,
-              egestas. Tempus aliquam, at fusce facilisi gravida lacus faucibus
-              felis, aliquet. Condimentum aenean nulla morbi euismod non vel
-              arcu sapien magna.
+              {item?.description
+                ? item.description
+                : "Description is not availiable for this courses"}
             </p>
           </Grid>
           <Grid item xs={4} md={4}>
-            <h3 style={{ textAlign: "right" }}>$19.99</h3>
+            <h3 style={{ textAlign: "right" }}>{item?.price + " $"}</h3>
           </Grid>
         </Grid>
         <Grid container spacing={2} className="topcourseuserGrid">
@@ -71,10 +112,10 @@ function TopCoursesComponent({ item }) {
             <div className="userprofilediv">
               <img src={avatar} className="avatar" alt="img" />
               <div className="div2">
-                <p className="p1">Emma Johns</p>
-                <p className="p2">PUBG player</p>
+                <p className="p1">{item?.creator?.user_id?.username}</p>
+                <p className="p2">{item?.course_name + " player"}</p>
                 <p className="p2">
-                  5.0 &nbsp;
+                  {item?.rating ? item.rating : "0.0"} &nbsp;
                   {[1, 2, 3, 4, 5].map((item) => (
                     <Star1
                       className="starID"
@@ -86,18 +127,27 @@ function TopCoursesComponent({ item }) {
                       }}
                     />
                   ))}
-                  &nbsp; (382,420)
+                  {"( " + countViews(item) + " )"}
+                  {/* &nbsp; (382,420) */}
                 </p>
               </div>
             </div>
           </Grid>
           <Grid item xs={6} md={6} className="topcourseuserRightGrid">
             <div style={{ textAlign: "right" }}>
-              <span className="marginRight">Advanced Level</span>
+              <span className="marginRight">
+                {item?.level ? item.level : "inital level"}
+              </span>
               <span className="marginRight">|</span>
-              <span className="marginRight">50 min</span>
+              <span className="marginRight">
+                {countTime(item) ? countTime(item) + "min" : "0 min"}
+              </span>
               <span className="marginRight">|</span>
-              <span className="marginRight">244 Student</span>
+              <span className="marginRight">
+                {item?.student
+                  ? item.student.length + " Student"
+                  : 0 + "   Student"}
+              </span>
             </div>
           </Grid>
         </Grid>
