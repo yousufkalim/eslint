d9cab1eaa8e5694000b41206dc7473f8c7938f24
falconitconsultant <sadamramzan@falconit.com.pk>
2022-03-18 05:16:30 -0700

    solve issues

--- src/components/PopupForms/ContentRequest.js
+++ src/components/PopupForms/ContentRequest.js
@@ -1,18 +1,44 @@
 import React from "react";
 import Dialog from "@mui/material/Dialog";
+import ClearIcon from "@mui/icons-material/Clear";
+
 const ContentRequest = ({ openContentRequest, setOpenContentRequest }) => {
-  //   useEffect(() => {
-  //     if (!openContentRequest) {
-  //       setValues({ username: "", email: "", password: "" });
-  //     }
-  //   }, []);
   const handleClose = () => {
     setOpenContentRequest(false);
   };
   return (
     <>
       <Dialog open={openContentRequest} onClose={handleClose}>
-        <div>ContentRequest</div>
+        <div className="requestMainDiv">
+          <div className="requestCenterDiv">
+            {/* /* --------------------------------- oneDiv --------------------------------- */}
+            <div className="requestDiv-box">
+              <h1 className="requestDivH1">Request a Course Content</h1>
+              <ClearIcon className="requestDivIcon" onClick={handleClose} />
+            </div>
+            {/* /* --------------------------------- oneDiv --------------------------------- */}
+            <div className="requestDiv-box2">
+              <label htmlFor=""> Game name </label>
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="Course title here"
+              />
+            </div>
+            {/* /* --------------------------------- oneDiv --------------------------------- */}
+            <div className="requestDiv-box2">
+              <label htmlFor=""> Game Level </label>
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="Course title here"
+              />
+            </div>
+            {/* /* --------------------------------- oneDiv --------------------------------- */}
+          </div>
+        </div>
       </Dialog>
     </>
   );
