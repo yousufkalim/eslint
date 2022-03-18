e16a7ec2d1e2b828416bf1cadf0e8e0f8b3c23bf
falconitconsultant <sadamramzan@falconit.com.pk>
2022-03-18 03:12:42 -0700

    remove extra width

--- src/components/PrivacyPolicy/Introduction.js
+++ src/components/PrivacyPolicy/Introduction.js
@@ -33,14 +33,16 @@ const Introduction = () => {
               <h3 className="introHeadingH4">Quand :</h3>
               {showIcons ? (
                 <img
-                  src={ShowIcon}
+                  src={CloseIcon}
                   alt=""
                   className="introShowIcon"
-                  onClick={() => setShowIcons(!showIcons)}
+                  onClick={() => {
+                    setShowIcons(!showIcons);
+                  }}
                 />
               ) : (
                 <img
-                  src={CloseIcon}
+                  src={ShowIcon}
                   alt=""
                   className="introShowIcon"
                   onClick={() => setShowIcons(!showIcons)}
