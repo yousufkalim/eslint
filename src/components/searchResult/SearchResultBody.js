0df505a098c1bc760214aec64c99e4fdef1e3682
asfandyar <asfandyar@falconit.com.pk>
2022-04-15 15:47:17 +0500

    fix

--- src/components/searchResult/SearchResultBody.js
+++ src/components/searchResult/SearchResultBody.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import Categories from "./Categories";
 import GameType from "./GameType";
 import Plateforms from "./Plateforms";
