0b4dcd92b0ec0c07b2488ace1069994ab2778f88
Arooj Ishfaq <aroojishfaq@falconit.com.pk>
2022-04-27 15:14:28 +0500

    guest popup

--- src/components/PopupForms/GuestSignUpPopUp.js
+++ src/components/PopupForms/GuestSignUpPopUp.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
+import api from "../../api";
+import { Store, UpdateStore } from "../../StoreContext";
 import Dialog from "@mui/material/Dialog";
 
-const GuestSignUpPopUp = ({ open, setOpen }) => {
-  //   const showLogoutPopup = () => {
-  //     setLogin(true);
-  //     setOpen(false);
-  //   };
-
-  const submitForm = async (event) => {
-    console.log("event", event);
+const GuestSignUpPopUp = ({ open, setOpen, setOpenSignup }) => {
+  const updateStore = UpdateStore();
+  const { user } = Store();
+  //   const { singlCourse } = props;
+  const handleClick = async (u, course) => {
+    const data = {
+      courseId: course?._id,
+      userId: u?._id,
+    };
+    let res = await api("post", "/users/enrolledCourseAndStarted", data);
+    if (res) {
+      updateStore({ user: res.data });
+    }
   };
+
   const handleClose = () => {
     setOpen(false);
   };
@@ -19,42 +27,56 @@ const GuestSignUpPopUp = ({ open, setOpen }) => {
     <>
       <div>
         <Dialog open={open} onClose={handleClose}>
-          <div
-            className="create_container"
-            // style={{ height: "100%", width: "50%" }}
-          >
-            <h3> You must be a registered user to continue </h3>
-            <button
-              className="formbtn2"
-              type="submit"
-              onClick={submitForm}
+          <div className="create_container" style={{ padding: "6%" }}>
+            <h1
+              className="text-center"
               style={{
-                width: "60%",
-                marginLeft: "20%",
-                marginTop: "6%",
-                marginBottom: "6%",
-                borderRadius: "8px",
-                fontSize: "30px",
+                color: "white",
+                letterSpacing: "1px",
+                textAlign: "center",
+                marginTop: "2%",
               }}
             >
-              Sign Up Now
-            </button>
-            <button
-              //   className="formbtn2"
-              type="submit"
-              onClick={submitForm}
-              style={{
-                width: "60%",
-                marginLeft: "20%",
-                marginTop: "6%",
-                marginBottom: "6%",
-                borderRadius: "8px",
-                fontSize: "30px",
-                background: "grey",
-              }}
-            >
-              Close
-            </button>
+              {" "}
+              You must be a registered user to continue{" "}
+            </h1>
+            <div style={{ display: "flex", paddingTop: "3%" }}>
+              <button
+                className="formbtn2"
+                onClick={() => {
+                  setOpenSignup(true);
+                }}
+                style={{
+                  width: "50%",
+                  marginLeft: "17%",
+                  marginTop: "6%",
+                  padding: "1%",
+                  marginBottom: "6%",
+                  borderRadius: "8px",
+                }}
+              >
+                Sign Up Now
+              </button>
+              <button
+                type="submit"
+                onClick={handleClose}
+                style={{
+                  width: "45%",
+                  marginLeft: "3%",
+                  marginRight: "20%",
+                  marginTop: "6%",
+                  padding: "1%",
+                  fontSize: "20px",
+                  marginBottom: "6%",
+                  borderRadius: "8px",
+                  border: "none",
+                  background: "rgba(128, 128, 128, 0.562)",
+                  color: "white",
+                }}
+              >
+                Close
+              </button>
+            </div>
           </div>
         </Dialog>
       </div>
