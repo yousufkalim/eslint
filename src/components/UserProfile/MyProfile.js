254cedd888a78792cfc111c190437e1375facd53
Arooj Ishfaq <aroojishfaq@falconit.com.pk>
2022-05-09 21:58:34 +0500

    login/logout bugs+functionalities

--- src/components/UserProfile/MyProfile.js
+++ src/components/UserProfile/MyProfile.js
@@ -2,8 +2,15 @@ import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import MyProfileData from "./MyProfileData";
 import MyPrifileSetting from "./MyPrifileSetting";
+import { Store, UpdateStore } from "../../StoreContext";
+import { useHistory } from "react-router-dom";
+import api from "../../api/index";
+
 const MyProfile = (props) => {
   const { user } = props;
+  const updateStore = UpdateStore();
+  const history = useHistory();
+
   const [profileBtn, setProfileBtn] = useState(1);
   const [a, seta] = useState(false);
   let btnTags = [
@@ -42,6 +49,14 @@ const MyProfile = (props) => {
 
   const handeClick = (i) => {
     setProfileBtn(i);
+  };
+  const handleLogout = async () => {
+    let res = await api("post", "/users/logout/all");
+    if (res) {
+      updateStore({ user: null, creator: null });
+      localStorage.removeItem("token");
+      history.push("/home");
+    }
   };
 
   return (
@@ -87,6 +102,29 @@ const MyProfile = (props) => {
           ) : (
             <></>
           )}
+
+          <button
+            className="formbtn2"
+            type="submit"
+            onClick={handleLogout}
+            style={{
+              padding: "5px",
+              background:
+                "linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),linear-gradient(65.06deg, #662f88 9.05%, #20bf55 131.69%)",
+              color: "white",
+              fontWeight: "900",
+              borderRadius: " 3px",
+              cursor: "pointer",
+              border: "none",
+              width: " 11%",
+              fontSize: "15px",
+              display: "block",
+              fontWeight: "400",
+              marginLeft: "4px",
+            }}
+          >
+            Log out
+          </button>
         </div>
       </div>
     </>
