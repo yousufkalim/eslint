01bb9c8cfd4b040b8ac1fd58d6201725b310c080
Yousuf Kalim <yousuf@falconit.com.pk>
2022-06-30 16:57:10 +0500

    lint updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,10 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "airbnb", "airbnb/hooks"],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
+    ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react"],
-  rules: { "react/react-in-jsx-scope": "off" },
+  rules: {},
 };
